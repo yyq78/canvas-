d631ddd87f22b043d8ff4ca4182d9349e006cea8
yyq <1453582072@qq.com>
2018-02-28 16:31:11 +0800

    绚丽的时钟动画

--- 绘制数字时钟/countdown.js
+++ 绘制数字时钟/countdown.js
@@ -7,8 +7,11 @@ var R=8;
 var MARGIN_TOP=60;
 var MARGIN_LEFT=30;
 
-const endTime=new Date(2018,1,28,16,51,20);
+const endTime=new Date(2018,1,28,20,30,20);
 var curShowTimeSeconds=0;
+
+var balls=[];
+const colors = ["#33B5E5","#0099CC","#AA66CC","#9933CC","#99CC00","#669900","#FFBB33","#FF8800","#FF4444","#CC0000"];
 
 window.onload=function(){
     var canvas=document.getElementById("canvas");
@@ -25,6 +28,76 @@ window.onload=function(){
     );
 };
 
+function getCurrentShowTimeSeconds(){
+    var curTime=new Date();
+    var ret=endTime.getTime()-curTime.getTime();
+    ret=Math.round(ret/1000);
+    return ret>=0?ret:0;
+}
+function update(){
+    var nextShowTimeSeconds=getCurrentShowTimeSeconds();
+
+    var nextHours=parseInt(nextShowTimeSeconds/3600);
+    var nextMinutes=parseInt((nextShowTimeSeconds-nextHours*3600)/60);
+    var nextSeconds=nextShowTimeSeconds%60;
+
+    var curHours=parseInt(curShowTimeSeconds/3600);
+    var curMinutes=parseInt(curShowTimeSeconds-curHours*3600)/60;
+    var curSeconds=curShowTimeSeconds%60;
+
+    if(nextSeconds!=curSeconds){
+        if(parseInt(curHours/10)!=parseInt(nextHours/10)){
+            addBalls(MARGIN_LEFT,MARGIN_TOP,parseInt(curHours/10));
+        }
+        if(parseInt(curHours%10)!=parseInt(nextHours%10)){
+            addBalls(MARGIN_LEFT+15*(R+1),MARGIN_TOP,parseInt(curHours%10));
+        }
+        if(parseInt(curMinutes/10)!=parseInt(nextMinutes/10)){
+            addBalls(MARGIN_LEFT+39*(R+1),MARGIN_TOP,parseInt(curMinutes/10));
+        }
+        if(parseInt(curMinutes%10)!=parseInt(nextMinutes%10)){
+            addBalls(MARGIN_LEFT+54*(R+1),MARGIN_TOP,parseInt(curMinutes%10));
+        }
+        if(parseInt(curSeconds/10)!=parseInt(nextSeconds/10)){
+            addBalls(MARGIN_LEFT+78*(R+1),MARGIN_TOP,parseInt(curMinutes/10));
+        }
+        if(parseInt(curSeconds%10)!=parseInt(nextSeconds%10)){
+            addBalls(MARGIN_LEFT+93*(R+1),MARGIN_TOP,parseInt(curMinutes%10));
+        }
+        curShowTimeSeconds=nextShowTimeSeconds;
+    }
+    updateBalls();
+}
+function updateBalls(){
+    for(var i=0;i<balls.length;i++){
+        balls[i].x+=balls[i].vx;
+        balls[i].y+=balls[i].vy;
+        balls[i].vy+=balls[i].g;
+
+        if(balls[i].y>=WINDOW_HEIGHT-R){
+            balls[i].y=WINDOW_HEIGHT-R;
+            balls[i].vy=-balls[i].vy*0.75;
+        }
+    }
+}
+function addBalls(x,y,num) {
+    for (var i = 0; i < digit[num].length; i++)
+        for (var j = 0; j < digit[num][i].length; j++)
+            if (digit[num][i][j] == 1) {
+                var aBall = {
+                    x: x + j *2*(R + 1) + (R + 1),
+                    y: y + i *2*(R + 1) + (R + 1),
+                    g: 1.5 + Math.random(),
+                    vx: Math.pow(-1, Math.ceil(Math.random() * 1000)) * 4,
+                    vy: -10,
+                    color: colors[Math.floor(Math.random() * colors.length)]
+                }
+                balls.push(aBall);
+            }
+
+
+
+}
 function render(cxt){
     cxt.clearRect(0,0,WINDOW_WIDTH,WINDOW_HEIGHT);
     var hours=parseInt(curShowTimeSeconds/3600);
@@ -38,6 +111,16 @@ function render(cxt){
     renderDigit(MARGIN_LEFT+69*(R+1),MARGIN_TOP,10,cxt);
     renderDigit(MARGIN_LEFT+78*(R+1),MARGIN_TOP,parseInt(seconds/10),cxt);
     renderDigit(MARGIN_LEFT+93*(R+1),MARGIN_TOP,parseInt(seconds%10),cxt);
+
+    for(var i=0;i<balls.length;i++){
+        cxt.fillStyle=balls[i].color;
+
+        cxt.beginPath();
+        cxt.arc(balls[i].x,balls[i].y,R,0,2*Math.PI,true);
+        cxt.closePath();
+
+        cxt.fill();
+    }
 }
 function renderDigit(x,y,num,cxt){
     cxt.fillStyle="rgb(0,102,153)";
@@ -52,24 +135,8 @@ function renderDigit(x,y,num,cxt){
         }
     }
 }
-function getCurrentShowTimeSeconds(){
-    var curTime=new Date();
-    var ret=endTime.getTime()-curTime.getTime();
-    ret=Math.round(ret/1000);
-    return ret>=0?ret:0;
-}
-function update(){
-    var nextShowTimeSeconds=getCurrentShowTimeSeconds();
 
-    var nextHours=parseInt(nextShowTimeSeconds/3600);
-    var nextMinutes=parseInt((nextMinutes-nextHours*3600)/60);
-    var nextSeconds=nextShowTimeSeconds%60;
 
-    var curHours=parseInt(curShowTimeSeconds/3600);
-    var curMinutes=parseInt(curShowTimeSeconds-curHours*3600)/60;
-    var curSeconds=curShowTimeSeconds%60;
 
-    if(nextSeconds!=curSeconds){
-        curShowTimeSeconds=nextShowTimeSeconds;
-    }
-}
+
+
